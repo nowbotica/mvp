4a3e9be86c1c02699109dd2f89afcbe78163753d
midwinterboxes <office@binarygeometry.co.uk>
2017-11-24 13:39:34 +0000

    added some outline ui- directives

--- application/system/Listing.js
+++ application/system/Listing.js
@@ -19,23 +19,219 @@ function ListingService($http, $q) {
   function getListings() {
 
         var deferred = $q.defer();
-        $http({
-            method: 'GET',
-            url: window.mvpmSystemApiUrl,
-            params: {
-              action:   "mvpm_get_listing",
-              security: window.mvpmSystemSecurity
-            },
-            headers : {
-                'Content-Type' : 'application/json' 
-            },
-            }).success(function(data, status) {
-                deferred.resolve(data);
+        // $http({
+        //     method: 'GET',
+        //     url: window.mvpmSystemApiUrl,
+        //     params: {
+        //       action:   "mvpm_get_listing",
+        //       security: window.mvpmSystemSecurity
+        //     },
+        //     headers : {
+        //         'Content-Type' : 'application/json' 
+        //     },
+        //     }).success(function(data, status) {
+        //         deferred.resolve(data);
 
-            }).error(function(data, status) {
-                deferred.reject();
-        });
-        
+        //     }).error(function(data, status) {
+        //         deferred.reject();
+        // });
+        var j = {
+    "data": [
+        {
+            "name":"Toasted Bread Van",
+            "slug":"toasted-bread-van",
+            "post_id":"18",
+            "author_id":"1",
+            "cost":"4",
+            "description":"Lorem",
+            "duration":"20",
+            "last_updated":"2017-11-23 16:38:25",
+            "location":"London",
+            "rating":"1"
+        },
+        {
+            "name":"Toasted Bread Van",
+            "slug":"toasted-bread-van",
+            "post_id":"18",
+            "author_id":"1",
+            "cost":"4",
+            "description":"Lorem",
+            "duration":"45",
+            "last_updated":"2017-11-22 16:38:25",
+            "location":"Seoul",
+            "rating":"2"
+        },
+        {
+            "name":"Toasted Bread Van",
+            "slug":"toasted-bread-van",
+            "post_id":"18",
+            "author_id":"1",
+            "cost":"4",
+            "description":"Lorem",
+            "duration":"70",
+            "last_updated":"2017-11-13 16:38:25",
+            "location":"Buchurest",
+            "rating":"3"
+        },
+        {
+            "name":"Toasted Bread Van",
+            "slug":"toasted-bread-van",
+            "post_id":"18",
+            "author_id":"1",
+            "cost":"4",
+            "description":"Lorem",
+            "duration":"120",
+            "last_updated":"2017-10-23 16:38:25",
+            "location":"London",
+            "rating":"4"
+        },
+        {
+            "name":"Toasted Bread Van",
+            "slug":"toasted-bread-van",
+            "post_id":"18",
+            "author_id":"1",
+            "cost":"6",
+            "description":"Lorem",
+            "duration":"20",
+            "last_updated":"2017-10-13 16:38:25",
+            "location":"Seoul",
+            "rating":"5"
+        },
+        {
+            "name":"Toasted Bread Van",
+            "slug":"toasted-bread-van",
+            "post_id":"18",
+            "author_id":"1",
+            "cost":"6",
+            "description":"Lorem",
+            "duration":"45",
+            "last_updated":"2017-10-03 16:38:25",
+            "location":"Buchurest",
+            "rating":"3"
+        },
+        {
+            "name":"Toasted Bread Van",
+            "slug":"toasted-bread-van",
+            "post_id":"18",
+            "author_id":"1",
+            "cost":"6",
+            "description":"Lorem",
+            "duration":"70",
+            "last_updated":"2017-01-23 16:38:25",
+            "location":"London",
+            "rating":"1"
+        },
+        {
+            "name":"Toasted Bread Van",
+            "slug":"toasted-bread-van",
+            "post_id":"18",
+            "author_id":"1",
+            "cost":"6",
+            "description":"Lorem",
+            "duration":"120",
+            "last_updated":"2017-02-23 16:38:25",
+            "location":"Seoul",
+            "rating":"2"
+        },
+            {
+            "name":"Toasted Bread Van",
+            "slug":"toasted-bread-van",
+            "post_id":"18",
+            "author_id":"1",
+            "cost":"8",
+            "description":"Lorem",
+            "duration":"20",
+            "last_updated":"2017-03-23 16:38:25",
+            "location":"Buchurest",
+            "rating":"3"
+        },
+        {
+            "name":"Toasted Bread Van",
+            "slug":"toasted-bread-van",
+            "post_id":"18",
+            "author_id":"1",
+            "cost":"8",
+            "description":"Lorem",
+            "duration":"45",
+            "last_updated":"2017-04-23 16:38:25",
+            "location":"London",
+            "rating":"4"
+        },
+        {
+            "name":"Toasted Bread Van",
+            "slug":"toasted-bread-van",
+            "post_id":"18",
+            "author_id":"1",
+            "cost":"8",
+            "description":"Lorem",
+            "duration":"70",
+            "last_updated":"2016-11-23 16:38:25",
+            "location":"Seoul",
+            "rating":"4"
+        },
+        {
+            "name":"Toasted Bread Van",
+            "slug":"toasted-bread-van",
+            "post_id":"18",
+            "author_id":"1",
+            "cost":"8",
+            "description":"Lorem",
+            "duration":"120",
+            "last_updated":"2016-10-23 16:38:25",
+            "location":"Buchurest",
+            "rating":"4"
+        },
+        {
+            "name":"Toasted Bread Van",
+            "slug":"toasted-bread-van",
+            "post_id":"18",
+            "author_id":"1",
+            "cost":"10",
+            "description":"Lorem",
+            "duration":"20",
+            "last_updated":"2016-10-22 16:38:25",
+            "location":"Buchurest",
+            "rating":"5"
+        },
+        {
+            "name":"Toasted Bread Van",
+            "slug":"toasted-bread-van",
+            "post_id":"18",
+            "author_id":"1",
+            "cost":"10",
+            "description":"Lorem",
+            "duration":"45",
+            "last_updated":"2016-11-21 16:38:25",
+            "location":"London",
+            "rating":"2"
+        },
+        {
+            "name":"Toasted Bread Van",
+            "slug":"toasted-bread-van",
+            "post_id":"18",
+            "author_id":"1",
+            "cost":"10",
+            "description":"Lorem",
+            "duration":"70",
+            "last_updated":"2017-11-10 16:38:25",
+            "location":"London",
+            "rating":"4"
+        },
+        {
+            "name":"Toasted Bread Van",
+            "slug":"toasted-bread-van",
+            "post_id":"18",
+            "author_id":"1",
+            "cost":"10",
+            "description":"Lorem",
+            "duration":"120",
+            "last_updated":"2017-10-12 16:38:25",
+            "location":"London",
+            "rating":"3"
+        }
+    ]
+}
+deferred.resolve(j['data']);
         return deferred.promise;
   }
   return {
--- application/system/directives.js
+++ application/system/directives.js
@@ -1,4 +1,4 @@
-// console.log('shared directives go here');
+console.log('shared directives go here');
 MvpmApp.directive('systemLoader', function() {
     return {
         restrict: 'AE',
@@ -7,3 +7,148 @@ MvpmApp.directive('systemLoader', function() {
         template: '<article ng-hide="!isRouteLoading" class="row loader-wrap"><div class="loader">Loading...</div></article>'
     };
 });
+
+MvpmApp.directive('inputRating', function() {
+    return {
+        templateUrl: mvpmPartialsPath+'/directives/input-rating.html',
+        scope: {
+      		rated: '='
+    	},
+        controller: function($scope){
+		  // hard coded for brevity, we tell our app that max rating is 5
+		  $scope.lives = [1,2,3,4,5];
+		  $scope.howSmokey = $scope.rated;
+
+		  // smokeHere(smokes) {
+		    // this.howSmokey = smokes;//this.currentLife;
+
+		    // console.log('typs', this.howSmokey);
+		  // }
+        }
+    };
+});
+
+MvpmApp.directive('filterLocation', function() {
+    return {
+        templateUrl: mvpmPartialsPath+'/directives/filter-location.html',
+        scope: {
+      		rated: '='
+    	},
+        controller: function($scope){
+			  $scope.choices = [
+			  	{name:'London',value:'London'},
+			  	{name:'Seoul',value:'Seoul'},
+			  	{name:'Buchurest',value:'Buchurest'}
+			  ];
+        }
+    };
+});
+
+MvpmApp.directive('filterDuration', function() {
+    return {
+        templateUrl: mvpmPartialsPath+'/directives/filter-duration.html',
+        scope: {
+      		rated: '='
+    	},
+        controller: function($scope){
+			  $scope.durations = [
+			  	{name:'20 mins',value:'20 mins'},
+			  	{name:'45 mins',value:'45 mins'},
+			  	{name:'70 mins',value:'70 mins'},
+			  	{name:'120 mins',value:'120 mins'}
+			  ];
+        }
+    };
+});
+
+MvpmApp.directive('filterCost', function() {
+    return {
+        templateUrl: mvpmPartialsPath+'/directives/filter-cost.html',
+        scope: {
+      		rated: '='
+    	},
+        controller: function($scope){
+			$scope.config = {
+				min: 2,
+				max: 8,
+				step: 2
+			}
+        }
+    };
+});
+
+MvpmApp.directive('inputStar', function() {
+    return {
+        templateUrl: mvpmPartialsPath+'/directives/input-star.html',
+        scope: {
+      		rated: '='
+    	},
+        controller: function($scope){
+        	var boolgen = Math.round(Math.random());
+        	// if (boolgen == 1)
+		  	$scope.starRed = boolgen;
+
+		  // starThis() {
+		  //   this.starRed = !this.starRed;
+
+		  //   console.log('typs', this.starRed);
+		  // }
+
+
+		  // selectionChange(input: HTMLInputElement) {
+		  //   input.checked === true ? this.callApi(true) : this.callApi(false);
+		  // }
+
+		  // callApi(value){
+		  //   console.log('calling api', value)
+		  // }
+        }
+    };
+});
+
+
+
+//     trigger('smoke', [
+//       transition('*=>*', [
+//         query(':enter', style({ opacity: 0, zoom:.05 }), {optional: true}),
+
+//         query(':enter', stagger('300ms', [
+//           animate('.6s ease-in', keyframes([
+//             style({opacity: 0, zoom: .6,  offset: 0}),
+//             style({opacity: .5, zoom: 1.3, offset: .3}),
+//             style({opacity: 1, zoom: 1,  offset: 1}),
+//           ]))
+//         ]), {optional: true})
+//         ,
+//         query(':leave', stagger('300ms', [
+//           animate('.6s ease-out', keyframes([
+//             style({opacity: 1, zoom:1, offset: 0}),
+//             style({opacity: .5, zoom:1.3,  offset: 0}),
+//             style({opacity: 0, zoom:.6, offset: 0}),
+
+// function RatingCtrl(){
+// }
+
+  // animations: [
+  //   trigger('shine', [
+  //     transition('*=>*', [
+  //       query(':enter', style({ opacity: 0, zoom:.05 }), {optional: true}),
+
+  //       query(':enter', stagger('300ms', [
+  //         animate('.6s ease-in', keyframes([
+  //           style({opacity: 0, zoom: .6,  offset: 0}),
+  //           style({opacity: .5, zoom: 1.3, offset: .3}),
+  //           style({opacity: 1, zoom: 1,  offset: 1}),
+  //         ]))
+  //       ]), {optional: true})
+  //       ,
+  //       query(':leave', stagger('300ms', [
+  //         animate('.6s ease-out', keyframes([
+  //           style({opacity: 1, zoom:1, offset: 0}),
+  //           style({opacity: .5, zoom:1.3,  offset: 0}),
+  //           style({opacity: 0, zoom:.6, offset: 0}),
+  //         ]))
+  //       ]), {optional: true})
+  //     ])
+  //   ])
+  // ]
