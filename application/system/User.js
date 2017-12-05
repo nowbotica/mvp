8b1243abaac5f01053fd5854c7816d94f60a38f2
midwinterboxes <office@binarygeometry.co.uk>
2017-12-05 16:00:46 +0000

    created registration flow

--- application/system/User.js
+++ application/system/User.js
@@ -1,35 +1,46 @@
 /*
 * http://natko.com/wordpress-ajax-login-without-a-plugin-the-right-way/
+* // http://code.realcrowd.com/on-the-bleeding-edge-advanced-angularjs-form-validation/
 /*
 * Renders a dialog box at the top of the page
 * <user-context-menu></user-context-menu>
 */
-MvpmApp.controller("testUserCtrl", function($scope, UserService, LoggedinService) {
-    $scope.formData = {};
-    // console.log($scope.$parent);
-    $scope.$parent
-    $scope.formLogin = function() { 
-        $scope.dataMessage = window.mvpmUserLoginloadingmessage; 
-        setTimeout(function(){
-            $scope.dataMessage = window.mvpmUserLoginloadingmessage; 
 
-        },100)      
-        UserService.loginUser($scope.formData.username, $scope.formData.password).then(function(data) {
-            // console.log('new',data, 
-                $scope.$parent.closeModal();
-            if (data.loggedin ){
-                console.log('parent scope', $scope.$parent) //.system.loggedin = true;
-                $scope.dataMessage = 'login succes, close modal below'; 
-                console.log(data);
+
+/*
+* Handles the user interaction with the create membership form 
+// console.log('parent scope', $scope.$parent) //.system.loggedin = true;
+*/
+MvpmApp.controller("createMembershipCtrl", function($scope, UserService, LoggedinService) {
+    $scope.formData = {};
+    console.log($scope.$parent)
+    $scope.doingsomething = false;
+    $scope.step = 'one';
+    $scope.createMember = function() { 
+        console.log('dsdfds',$scope.formData)
+        $scope.dataMessage = 'Validating Membership'; 
+        $scope.doingsomething = true;
+        UserService.createUser($scope.formData.username, $scope.formData.email, $scope.formData.password).then(function(data) {
+            if (!data.registered ){
+                $scope.doingsomething = false;
+                $scope.dataMessage = data.message;
+            } 
+            else if (!data.loggedin) {
+                $scope.doingsomething = false;
+                $scope.dataMessage = data.message; 
             } else {
-                $scope.dataMessage = 'failed to login'; 
+                $scope.step = 'two';
+                // $scope.$parent.closeModal();
             }
         });
     }
 });
 
-
-MvpmApp.directive('userCreateMembership', ['UserService', function(UserService) {
+/*
+* Renders the create membership form 
+* <create-membership></create-membership>
+*/
+MvpmApp.directive('createMembership', ['UserService', function(UserService) {
     return {
         restrict: 'AE',
         replace: true,
@@ -38,12 +49,15 @@ MvpmApp.directive('userCreateMembership', ['UserService', function(UserService) 
             'password': '&',
             'form': '&'
         },
-        controller: 'testUserCtrl',
-        template: '<p>mvpmPartialsPath+/user/create-membership.html</p>',
-        // templateUrl: mvpmPartialsPath+'/user/user-register.html',
+        controller: 'createMembershipCtrl',
+        templateUrl: mvpmPartialsPath+'/user/create-membership.html',
     }
 }]);
 
+/*
+* Renders a dialog box at the top of the page
+* <create-membership-modal></create-membership-modal>
+*/
 MvpmApp.directive('createMembershipModal', ['$parse', 'UserService', function($parse, UserService){
     return {
         replace: true,
@@ -80,6 +94,37 @@ MvpmApp.directive('createMembershipModal', ['$parse', 'UserService', function($p
 }]);
 
 
+/*
+* Handles the user interaction with the login form 
+*/
+MvpmApp.controller("userLoginCtrl", function($scope, UserService, LoggedinService) {
+    $scope.formData = {};
+    // console.log($scope.$parent);
+    $scope.$parent
+    $scope.formLogin = function() { 
+        $scope.dataMessage = window.mvpmUserLoginloadingmessage; 
+        setTimeout(function(){
+            $scope.dataMessage = window.mvpmUserLoginloadingmessage; 
+
+        },100)      
+        UserService.loginUser($scope.formData.username, $scope.formData.password).then(function(data) {
+            // console.log('new',data, 
+                $scope.$parent.closeModal();
+            if (data.loggedin ){
+                console.log('parent scope', $scope.$parent) //.system.loggedin = true;
+                $scope.dataMessage = 'login succes, close modal below'; 
+                console.log(data);
+            } else {
+                $scope.dataMessage = 'failed to login'; 
+            }
+        });
+    }
+});
+
+/*
+* Renders a dialog box at the top of the page
+* <user-login></user-login>
+*/
 MvpmApp.directive('userLogin', ['UserService', function(UserService) {
     return {
         restrict: 'AE',
@@ -89,11 +134,15 @@ MvpmApp.directive('userLogin', ['UserService', function(UserService) {
             'password': '&',
             'form': '&'
         },
-        controller: 'testUserCtrl',
+        controller: 'userLoginCtrl',
         templateUrl: mvpmPartialsPath+'/user/user-login.html',
     }
 }]);
 
+/*
+* Renders a dialog box at the top of the page
+* <login-modal-launcher></login-modal-launcher>
+*/
 MvpmApp.directive('loginModalLauncher', ['$parse', 'UserService', function($parse, UserService){
     return {
         replace: true,
@@ -156,15 +205,17 @@ MvpmApp.directive('loginModalLauncher', ['$parse', 'UserService', function($pars
     };
 }]);
 
-
+/*
+* Legacy handling of user actions
+*/
 MvpmApp.controller("UserActionsCtrl", function($scope, UserService, LoggedinService) {
-    $scope.showLogin = function(){
-        console.log('show login', $scope.$parent.system.loggedin)
-    };
-    $scope.showRegister = function(){
-        console.log('register')
+    // $scope.showLogin = function(){
+    //     console.log('show login', $scope.$parent.system.loggedin)
+    // };
+    // $scope.showRegister = function(){
+    //     console.log('register')
 
-    };
+    // };
     $scope.userLogout = function(){
         UserService.logoutUser().then(function(data) {
             $scope.$parent.system.loggedin = 'loggedout';
@@ -179,7 +230,6 @@ MvpmApp.controller("UserActionsCtrl", function($scope, UserService, LoggedinServ
 * MvpmApp.controller('ListingCtrl', UserCtrl);
 * MvpmApp.factory('ListingService', UserService);
 */
-
 MvpmApp.service('UserService', UserService);
 function UserService($http, $q) {
     function loginUser(username, password) {
@@ -238,87 +288,43 @@ function UserService($http, $q) {
         });
 
         return deferred.promise;
-
     }
-    // would a factory return loginUser()?
+    function createUser(username, email, password) {
+        var that = this;
+        var deferred = $q.defer();
+        console.log('creating with', username, email, password)
+        that.username = username;
+        that.email    = email;
+        that.password = password;
+
+        $http({
+            method: 'POST',
+            url: window.mvpmSystemApiUrl,
+            params: {
+                action:   "mvpm_user_create",
+                security: window.mvpmSystemSecurity,
+                username: that.username,
+                email:    that.email,
+                password: that.password,
+                remember: true
+            },
+            headers : {
+                'Content-Type' : 'application/json'
+            }
+        }).success(function(data, status) {
+            console.log('testable data', data);
+            deferred.resolve(data);
+
+        }).error(function(data, status) {
+            // console.log('error data', data);
+            deferred.reject();
+        });
+
+        return deferred.promise;
+    }
     return {
         loginUser: loginUser,
-        logoutUser: logoutUser
+        logoutUser: logoutUser,
+        createUser: createUser
     };
 }
-// http://code.realcrowd.com/on-the-bleeding-edge-advanced-angularjs-form-validation/
-
-
-
-// // UserLoginShow();
-// // $('a#show_login').on('click', function(e){
-// //     $('body').prepend('<div class="login_overlay"></div>');
-// //     $('form#login').fadeIn(500);
-// //     $('div.login_overlay, form#login a.close').on('click', function(){
-// //         $('div.login_overlay').remove();
-// //         $('form#login').hide();
-// //     });
-// //     e.preventDefault();
-// // });
-
-
-// /*
-// * UserService($http, $q)
-// *$.ajax({
-// *    type: 'POST',
-// *    dataType: 'json',
-// *    url: window.mvpmSystemApiUrl+'/',
-// *    data: {
-// *        'action': 'ajaxlogin', //calls wp_ajax_nopriv_ajaxlogin
-// *        'username': $('form#login #username').val(),
-// *        'password': $('form#login #password').val(),
-// *        'security': $('form#login #security').val() },
-// *    }
-// *});
-// *loginUser(username, $('form#login #password').val()
-// *username, password
-// */
-
-// function createOverlay(){
-// $('body').prepend('<div class="login_overlay"></div>');
-// $('form#login').fadeIn(500);
-// $('div.login_overlay, form#login a.close').on('click', function(){
-//     $('div.login_overlay').remove();
-//     $('form#login').hide();
-// });
-// }
-// #
-// #//
-// app.directive('myChange_user_usernameinput', function() {
-//     return {
-//         link:  function link(scope, element) {
-//             element.bind('change', function() {
-//                 alert('change on ' + element);
-//             });
-//         }
-//         template: '<div class="mvpm-input"><input type="text" ng-model="" placeholder></div>';
-//     }
-// });
-//
-// app.directive('myChange_passwordinput', function() {
-//     return {
-//         link:  function link(scope, element) {
-//             element.bind('change', function() {
-//                 alert('change on ' + element);
-//             });
-//         }
-//     }
-// });
-//
-//
-// app.directive('myChange_loginsubmitinput', function() {
-//     return {
-//         link:  function link(scope, element) {
-//             element.bind('change', function() {
-//                 alert('change on ' + element);
-//             });
-//         }
-//     }
-// });
-// The above directive can be used as follows:
-
